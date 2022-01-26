7d4d2e785dff52468814ed9bb9696c13c399dca5
jamesmakau18@gmail.com <jamesmakau18@gmail.com>
2022-01-26 16:07:50 +0300

    first commit

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+importScripts("/precache-manifest.1d064c729b7e1ec3e1082e4869c3a254.js", "/workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/workbox-v4.3.1"});
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.suppressWarnings()
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.staleWhileRevalidate(0),
+)
+
+workbox.routing.registerRoute(
+  new RegExp('https://reqres.in'),
+  workbox.strategies.networkFirst(),
+)
+
