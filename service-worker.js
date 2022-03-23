90157317cbe56422793f3941d2511c5e8fb1c98e
jamesmakau18@gmail.com <jamesmakau18@gmail.com>
2022-03-23 16:40:00 +0300

    Card

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.c9e943ef85a44b0fd6ab0568da3688b5.js", "/workbox-v4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.1d065777bbd36a2b1d1bd38e35fe81fe.js", "/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v4.3.1"});
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.suppressWarnings()
